079642dca1d7c8f664dca616e0e481e4c2b5e47e
CanaliDavide <79902786+CanaliDavide@users.noreply.github.com>
2022-07-17 16:03:42 +0200

    terrain and class structure

--- Hungry_Bird_Project/MyProject.cpp
+++ Hungry_Bird_Project/MyProject.cpp
@@ -17,6 +17,192 @@ struct UniformBufferObject {
 	alignas(16) glm::mat4 model;
 };
 
+class SkyBox {
+	protected:
+	Pipeline P_SkyBox;
+	Model M_skyBox;
+	Texture T_skyBox;
+	DescriptorSet DS_skyBox;
+
+	public:
+	void init(BaseProject *bp, DescriptorSetLayout DSLobj, DescriptorSetLayout DSLglobal) {
+		P_SkyBox.init(bp, "shaders/skyBoxVert.spv", "shaders/skyBoxFrag.spv", { &DSLglobal, &DSLobj });
+		M_skyBox.init(bp, MODEL_PATH + "/SkyBox/SkyBox.obj");
+		T_skyBox.init(bp, TEXTURE_PATH + "/SkyBox/SkyBox.png");
+		DS_skyBox.init(bp, &DSLobj, {
+						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
+						{1, TEXTURE, 0, &T_skyBox}
+			});
+	}
+
+	void cleanup() {
+		DS_skyBox.cleanup();
+		T_skyBox.cleanup();
+		M_skyBox.cleanup();
+		P_SkyBox.cleanup();
+	}
+
+	void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage, DescriptorSet DS_global) {
+		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+			P_SkyBox.graphicsPipeline);
+
+		vkCmdBindDescriptorSets(commandBuffer,
+			VK_PIPELINE_BIND_POINT_GRAPHICS,
+			P_SkyBox.pipelineLayout, 0, 1, &DS_global.descriptorSets[currentImage],
+			0, nullptr);
+
+		VkBuffer vertexBuffers_skyBox[] = { M_skyBox.vertexBuffer };
+		VkDeviceSize offsets_skyBox[] = { 0 };
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers_skyBox, offsets_skyBox);
+		vkCmdBindIndexBuffer(commandBuffer, M_skyBox.indexBuffer, 0,
+			VK_INDEX_TYPE_UINT32);
+		vkCmdBindDescriptorSets(commandBuffer,
+			VK_PIPELINE_BIND_POINT_GRAPHICS,
+			P_SkyBox.pipelineLayout, 1, 1, &DS_skyBox.descriptorSets[currentImage],
+			0, nullptr);
+		vkCmdDrawIndexed(commandBuffer,
+			static_cast<uint32_t>(M_skyBox.indices.size()), 1, 0, 0, 0);
+	}
+
+	UniformBufferObject update(UniformBufferObject ubo) {
+		ubo.model = glm::scale(glm::mat4(1.0f), glm::vec3(50.0f, 50.0f, 50.0f));
+		return ubo;
+	}
+	
+	void updateUniformBuffer(VkDevice device, int currentImage, void *data, UniformBufferObject ubo){
+		ubo = update(ubo);
+		vkMapMemory(device, DS_skyBox.uniformBuffersMemory[0][currentImage], 0,
+		sizeof(ubo), 0, &data);
+		memcpy(data, &ubo, sizeof(ubo));
+		vkUnmapMemory(device, DS_skyBox.uniformBuffersMemory[0][currentImage]);
+	}
+};
+
+class Camera {
+	protected:
+	glm::vec3 CamPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 CamAng = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 CamDir;
+
+	const float ROT_SPEED = glm::radians(60.0f);
+	const float MOVE_SPEED = 1.75f;
+
+	public:
+	glm::mat4 update(GLFWwindow *window, float deltaT) {
+		// Camera movements
+		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
+			CamAng.y += deltaT * ROT_SPEED;
+		}
+		if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
+			CamAng.y -= deltaT * ROT_SPEED;
+		}
+		if (glfwGetKey(window, GLFW_KEY_UP)) {
+			CamAng.x += deltaT * ROT_SPEED;
+		}
+		if (glfwGetKey(window, GLFW_KEY_DOWN)) {
+			CamAng.x -= deltaT * ROT_SPEED;
+		}
+		if (glfwGetKey(window, GLFW_KEY_Q)) {
+			CamAng.z += deltaT * ROT_SPEED;
+		}
+		if (glfwGetKey(window, GLFW_KEY_E)) {
+			CamAng.z -= deltaT * ROT_SPEED;
+		}
+
+		glm::mat3 CamEye = glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.y, glm::vec3(0.0f, 1.0f, 0.0f))) *
+			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.x, glm::vec3(1.0f, 0.0f, 0.0f))) *
+			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.z, glm::vec3(0.0f, 0.0f, 1.0f)));
+
+		if (glfwGetKey(window, GLFW_KEY_A)) {
+			CamPos -= MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
+				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1, 0, 0, 1)) * deltaT;
+		}
+		if (glfwGetKey(window, GLFW_KEY_D)) {
+			CamPos += MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
+				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1, 0, 0, 1)) * deltaT;
+		}
+		if (glfwGetKey(window, GLFW_KEY_S)) {
+			CamPos += MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
+				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0, 0, 1, 1)) * deltaT;
+		}
+		if (glfwGetKey(window, GLFW_KEY_W)) {
+			CamPos -= MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
+				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0, 0, 1, 1)) * deltaT;
+		}
+		if (glfwGetKey(window, GLFW_KEY_F)) {
+			CamPos -= MOVE_SPEED * glm::vec3(0, 1, 0) * deltaT;
+		}
+		if (glfwGetKey(window, GLFW_KEY_R)) {
+			CamPos += MOVE_SPEED * glm::vec3(0, 1, 0) * deltaT;
+		}
+
+		return CamDir = glm::translate(glm::transpose(glm::mat4(CamEye)), -CamPos);
+	}
+
+};
+
+class Asset {
+	protected:
+	Model model;
+	Texture texture;
+	std::vector<DescriptorSet> dSetVector;
+
+	public:
+	void init(BaseProject *bp, std::string modelPath, std::string texturePath, DescriptorSetLayout DSLobj) {
+		model.init(bp, MODEL_PATH + modelPath);
+		texture.init(bp, TEXTURE_PATH + texturePath);
+		DescriptorSet dSet;
+		dSetVector.push_back(dSet);
+		dSetVector.back().init(bp, &DSLobj, {
+						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
+						{1, TEXTURE, 0, &texture}
+			});
+	}
+
+	void addDSet(BaseProject* bp, DescriptorSetLayout DSLobj, DescriptorSet dSet) {
+		dSetVector.push_back(dSet);
+		dSetVector.back().init(bp, &DSLobj, {
+						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
+						{1, TEXTURE, 0, &texture}
+			});
+	}
+
+	void cleanup() {
+		for (DescriptorSet dSet : dSetVector)
+		{
+			dSet.cleanup();
+		}
+		texture.cleanup();
+		model.cleanup();
+	}
+
+	void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage, DescriptorSet DS_global, Pipeline P1) {
+		VkBuffer vertexBuffers[] = { model.vertexBuffer };
+		VkDeviceSize offsets[] = { 0 };
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+		vkCmdBindIndexBuffer(commandBuffer, model.indexBuffer, 0,
+			VK_INDEX_TYPE_UINT32);
+		for (DescriptorSet dSet : dSetVector)
+		{
+			vkCmdBindDescriptorSets(commandBuffer,
+				VK_PIPELINE_BIND_POINT_GRAPHICS,
+				P1.pipelineLayout, 1, 1, &dSet.descriptorSets[currentImage],
+				0, nullptr);
+		}
+		vkCmdDrawIndexed(commandBuffer,
+			static_cast<uint32_t>(model.indices.size()), 1, 0, 0, 0);
+	}
+};
+
+class birdBlue{
+	protected:
+	DescriptorSet dSet;
+
+};
+
+class pigStd{
+
+};
 
 // MAIN ! 
 class MyProject : public BaseProject {
@@ -27,9 +213,11 @@ class MyProject : public BaseProject {
 	DescriptorSetLayout DSLglobal;
 	DescriptorSetLayout DSLobj;
 
+	Camera camera;
+	SkyBox skyBox;
+
 	// Pipelines [Shader couples]
 	Pipeline P1;
-	Pipeline P_SkyBox;
 
 	// Models, textures and Descriptors (values assigned to the uniforms)
 	Model M_blues;
@@ -40,16 +228,8 @@ class MyProject : public BaseProject {
 	Texture T_pig;
 	DescriptorSet DS_pig;
 
-	Model M_skyBox;
-	Texture T_skyBox;
-	DescriptorSet DS_skyBox;
 
 	DescriptorSet DS_global;
-
-	// Camera
-	glm::vec3 CamPos = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 CamAng = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::mat4 CamDir;
 	
 
 	
@@ -86,7 +266,7 @@ class MyProject : public BaseProject {
 		// The last array, is a vector of pointer to the layouts of the sets that will
 		// be used in this pipeline. The first element will be set 0, and so on..
 		P1.init(this, "shaders/materialVert.spv", "shaders/materialFrag.spv", {&DSLglobal, &DSLobj});
-		P_SkyBox.init(this, "shaders/skyBoxVert.spv", "shaders/skyBoxFrag.spv", {&DSLglobal, &DSLobj});
+		
 
 		// Models, textures and Descriptors (values assigned to the uniforms)
 		M_blues.init(this, MODEL_PATH + "/Birds/blues.obj");
@@ -110,13 +290,7 @@ class MyProject : public BaseProject {
 			});
 
 
-		M_skyBox.init(this, MODEL_PATH + "/SkyBox/SkyBox.obj");
-		T_skyBox.init(this, TEXTURE_PATH + "/SkyBox/SkyBox.png");
-		DS_skyBox.init(this, &DSLobj, {
-						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
-						{1, TEXTURE, 0, &T_skyBox}
-			});
-
+		skyBox.init(this, DSLobj, DSLglobal);
 
 
 		DS_global.init(this, &DSLglobal, {
@@ -136,12 +310,10 @@ class MyProject : public BaseProject {
 		T_pig.cleanup();
 		M_pig.cleanup();
 
-		DS_skyBox.cleanup();
-		T_skyBox.cleanup();
-		M_skyBox.cleanup();
+		skyBox.cleanup();
 
 		P1.cleanup();
-		P_SkyBox.cleanup();
+
 
 		DS_global.cleanup();
 
@@ -156,25 +328,7 @@ class MyProject : public BaseProject {
 
 		// --------------------- SKYBOX -------------------------
 
-		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
-			P_SkyBox.graphicsPipeline);
-
-		vkCmdBindDescriptorSets(commandBuffer,
-			VK_PIPELINE_BIND_POINT_GRAPHICS,
-			P_SkyBox.pipelineLayout, 0, 1, &DS_global.descriptorSets[currentImage],
-			0, nullptr);
-
-		VkBuffer vertexBuffers_skyBox[] = { M_skyBox.vertexBuffer };
-		VkDeviceSize offsets_skyBox[] = { 0 };
-		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers_skyBox, offsets_skyBox);
-		vkCmdBindIndexBuffer(commandBuffer, M_skyBox.indexBuffer, 0,
-			VK_INDEX_TYPE_UINT32);
-		vkCmdBindDescriptorSets(commandBuffer,
-			VK_PIPELINE_BIND_POINT_GRAPHICS,
-			P_SkyBox.pipelineLayout, 1, 1, &DS_skyBox.descriptorSets[currentImage],
-			0, nullptr);
-		vkCmdDrawIndexed(commandBuffer,
-			static_cast<uint32_t>(M_skyBox.indices.size()), 1, 0, 0, 0);
+		skyBox.populateCommandBuffer(commandBuffer, currentImage, DS_global);
 
 
 		// -------------------- Pipeline 1 -----------------------------
@@ -240,68 +394,12 @@ class MyProject : public BaseProject {
 		float deltaT = time - lastTime;
 		lastTime = time;
 					
-
-
 		UniformBufferObject ubo{};
 		GlobalUniformBufferObject gubo{};
 
 		void* data;
 	
-		const float ROT_SPEED = glm::radians(60.0f);
-		const float MOVE_SPEED = 1.75f;
-		
-		
-		// Camera movements
-		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
-			CamAng.y += deltaT * ROT_SPEED;
-		}
-		if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
-			CamAng.y -= deltaT * ROT_SPEED;
-		}
-		if (glfwGetKey(window, GLFW_KEY_UP)) {
-			CamAng.x += deltaT * ROT_SPEED;
-		}
-		if (glfwGetKey(window, GLFW_KEY_DOWN)) {
-			CamAng.x -= deltaT * ROT_SPEED;
-		}
-		if (glfwGetKey(window, GLFW_KEY_Q)) {
-			CamAng.z += deltaT * ROT_SPEED;
-		}
-		if (glfwGetKey(window, GLFW_KEY_E)) {
-			CamAng.z -= deltaT * ROT_SPEED;
-		}
-
-		glm::mat3 CamEye = glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.y, glm::vec3(0.0f, 1.0f, 0.0f))) *
-			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.x, glm::vec3(1.0f, 0.0f, 0.0f))) *
-			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.z, glm::vec3(0.0f, 0.0f, 1.0f)));
-		
-		if(glfwGetKey(window, GLFW_KEY_A)) {
-			CamPos -= MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
-				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1, 0, 0, 1)) *  deltaT;
-		}
-		if(glfwGetKey(window, GLFW_KEY_D)) {
-			CamPos += MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
-				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1, 0, 0, 1)) *  deltaT;
-		}
-		if(glfwGetKey(window, GLFW_KEY_S)) {
-			CamPos += MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
-				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0, 0, 1, 1)) * deltaT;
-		}
-		if(glfwGetKey(window, GLFW_KEY_W)) {
-			CamPos -= MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
-				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(0, 0, 1, 1)) * deltaT;
-		}
-		if(glfwGetKey(window, GLFW_KEY_F)) {
-			CamPos -= MOVE_SPEED * glm::vec3(0,1,0) * deltaT;
-		}
-		if(glfwGetKey(window, GLFW_KEY_R)) {
-			CamPos += MOVE_SPEED * glm::vec3(0,1,0) * deltaT;
-		}
-
-		CamDir = glm::translate(glm::transpose(glm::mat4(CamEye)), -CamPos);
-
-		
-		gubo.view = CamDir;
+		gubo.view = camera.update(window, deltaT);
 		gubo.proj = glm::perspective(glm::radians(45.0f),
 			swapChainExtent.width / (float)swapChainExtent.height,
 			0.1f, 200.0f);
@@ -316,57 +414,17 @@ class MyProject : public BaseProject {
 
 
 		// SkyBox
-		ubo.model = glm::scale(glm::mat4(1.0f), glm::vec3(50.0f, 50.0f, 50.0f));
-		vkMapMemory(device, DS_skyBox.uniformBuffersMemory[0][currentImage], 0,
-			sizeof(ubo), 0, &data);
-		memcpy(data, &ubo, sizeof(ubo));
-		vkUnmapMemory(device, DS_skyBox.uniformBuffersMemory[0][currentImage]);
+		
+		skyBox.updateUniformBuffer(device, currentImage, data ,ubo);
 
 
 		// Here is where you actually update your uniforms
 
 		// ------------------------ BIRD BLUES ---------------------------
-		static bool isJumping = false;
-		static bool isDescending = false;
-		static float startJump = 0.0f;
-		static float deltaTime_Jump = 0.0f;
-
-		float height = 2.0f;
-		float currentHeight = 0.0f;
-
-		static glm::mat4 birdPos = glm::mat4(1.0f);
-
-		if (glfwGetKey(window, GLFW_KEY_J)) {
-			if (!isJumping) {
-				startJump = time;
-				deltaTime_Jump = 0.0f;
-				isJumping = true;
-			}
-		}
-		if (isJumping)
-		{
-			deltaTime_Jump = time - startJump;
-			if (!isDescending) {
-				currentHeight = deltaTime_Jump * 1.0f;
-				birdPos = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, currentHeight, 0.0f));
-				if (currentHeight > height) {
-					startJump = time;
-					isDescending = true;
-				}
-			}
-			else
-			{
-				currentHeight = height - deltaTime_Jump * 1.0f;
-				birdPos = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, currentHeight, 0.0f));
-				if (currentHeight <= 0) {
-					isJumping = false;
-					isDescending = false;
-				}
-			}
-		}
 
 
-		ubo.model = birdPos;
+
+		ubo.model = glm::mat4(1.0f);
 		
 		vkMapMemory(device, DS_blues.uniformBuffersMemory[0][currentImage], 0,
 							sizeof(ubo), 0, &data);
